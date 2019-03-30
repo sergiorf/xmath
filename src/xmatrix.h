9c3634309e3d8a86336184f963a39711633d56a8
sergiorf <sergio.rodriguez.freire@gmail.com>
2019-03-30 18:11:48 -0300

    refactoring

--- src/xmatrix.h
+++ src/xmatrix.h
@@ -3,6 +3,9 @@ class XMatrix {
 public:
 	XMatrix(int rows, int cols);
 	~XMatrix();
+	inline int rows() const { return m_rows; }
+	inline int cols() const { return m_cols; }
+	inline double& at(int r, int c) const { return m_vals[m_cols * r + c]; }
 private:
 	const int m_rows;
 	const int m_cols;
---
+++ test/src/xmatrix_tests.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Sergio Rodriguez on 30/03/2019.
+//
+#include <xmatrix.h>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+
+class DividerTest : public ::testing::Test {
+
+protected:
+
+  virtual void SetUp() {
+  };
+
+  virtual void TearDown() {
+  };
+
+};
+
